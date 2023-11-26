bcff1640004d594491cf7f355f73fe06a9e8cc26
bsaglimb <bsaglimb>
2023-11-25 23:53:12 -0500

    starting to add to script

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+var key = '64f2ee2a8261daa4d9f780f5b365f275';
+var city = "Miami"
+
+//Grabs the current time and date
+var date = moment().format('dddd, MMMM Do YYYY');
+var dateTime = moment().format('YYYY-MM-DD HH:MM:SS')
